a62faccb00ee109c91c85ad32fb36ba13ca77286
CalebeSilveira <calebe_mendes.silveira@hotmail.com>
2021-03-18 21:05:44 -0300

    Frist Controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Web application from aspNet Core MVC";
+            ViewData["professor"] = "Nelio Alves";
 
             return View();
         }
